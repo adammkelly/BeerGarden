6e1f677a0e09011cb381781fe869779c3ac64b34
Adam Kelly <adam.kelly@plexxi.com>
2020-04-21 16:27:00 +0100

    Build and Deploy

--- docker/build.sh
+++ docker/build.sh
@@ -6,20 +6,34 @@ cd "$(dirname ${BASH_SOURCE[0]})"
 # Defines for script.
 IMAGE="beergarden"
 CONTAINER_NAME="beergarden"
+UI_IMAGE="beergarden_ui"
+UI_CONTAINER_NAME="beergarden_ui"
 VERSION="1"
 
 # Remove old container if it exists.
-docker rm -f beergarden > /dev/null 2>&1 && echo 'removed container' || echo 'nothing to remove'
+docker rm -f ${CONTAINER_NAME} > /dev/null 2>&1 && echo 'removed container' || echo 'nothing to remove'
+docker rm -f ${UI_CONTAINER_NAME} > /dev/null 2>&1 && echo 'removed container' || echo 'nothing to remove'
 
 # Build image if necessary.
-docker build -t beergarden .
+docker build -t ${IMAGE} .
+docker build -t ${UI_IMAGE} ui/
+
+# Build UI
+docker run --rm -it \
+--name=${UI_CONTAINER_NAME} \
+-v $(pwd)/..:/beergarden:cached \
+${UI_IMAGE}
+#docker rm -f ${UI_CONTAINER_NAME} > /dev/null 2>&1 && echo 'removed container' || echo 'nothing to remove'
 
 # Run the container.
 docker run -d -it \
 -e VERSION=$VERSION \
+-p 80:80  \
 --name=${CONTAINER_NAME} \
 -v $(pwd)/..:/beergarden:cached \
 ${IMAGE}
 
+docker exec -u 0 -ti beergarden bash -c 'cp /beergarden/docker/nginx.conf /etc/nginx/; /etc/init.d/nginx start'
+
 # Jump into docker container.
-docker exec -u 0 -ti beergarden bash
+docker exec -ti beergarden bash
