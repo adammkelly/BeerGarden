194a89cbaba850804c1cd04b2b1e2f44b959c6ff
Adam Kelly <adam.kelly@plexxi.com>
2020-04-21 17:27:20 +0100

    deps

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Button from 'react-bootstrap/Button'
 
 function App() {
   return (
@@ -18,6 +19,15 @@ function App() {
         >
           Learn React
         </a>
+        <>
+          <Button variant="primary">Primary</Button>{' '}
+          <Button variant="secondary">Secondary</Button>{' '}
+          <Button variant="success">Success</Button>{' '}
+          <Button variant="warning">Warning</Button>{' '}
+          <Button variant="danger">Danger</Button> <Button variant="info">Info</Button>{' '}
+          <Button variant="light">Light</Button> <Button variant="dark">Dark</Button>{' '}
+          <Button variant="link">Link</Button>
+        </>
       </header>
     </div>
   );
