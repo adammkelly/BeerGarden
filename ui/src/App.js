b3a383881ae33e3f1b1224da6056eee54faed8d0
Adam Kelly <adam.kelly@plexxi.com>
2020-04-22 13:33:58 +0100

    Figure out AJAX call on click

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,36 +1,53 @@
 import React from 'react';
-import logo from './logo.svg';
+import beer from './beer.svg';
 import './App.css';
-import Button from 'react-bootstrap/Button'
+import Button from 'react-bootstrap/Button';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <>
-          <Button variant="primary">Primary</Button>{' '}
-          <Button variant="secondary">Secondary</Button>{' '}
-          <Button variant="success">Success</Button>{' '}
-          <Button variant="warning">Warning</Button>{' '}
-          <Button variant="danger">Danger</Button> <Button variant="info">Info</Button>{' '}
-          <Button variant="light">Light</Button> <Button variant="dark">Dark</Button>{' '}
-          <Button variant="link">Link</Button>
-        </>
-      </header>
-    </div>
-  );
+function GetRandomBeer() {
+  console.log('STARTED HOOK - Figure oout fetch')
 }
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { };
+  }
+
+    render () {
+      return (
+        <div className="App">
+          <header className="App-header">
+            <img src={beer} className="Beer-logo" alt="beer" />
+            <p>
+              Edit <code>src/App.js</code> and save to reload.
+            </p>
+            <a
+              className="App-link"
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Learn React
+            </a>
+            <MyButton handleClick={GetRandomBeer}/>
+          </header>
+        </div>);
+    }
+  }
+
+  class MyButton extends React.Component {
+    constructor(props) {
+      super(props);
+      console.log(props)
+    }
+
+    render() {
+      return (
+        <div>
+          <Button onClick={this.props.handleClick} variant="primary">AAAAAH!</Button>
+        </div>
+      );
+    }
+  }
 
 export default App;
